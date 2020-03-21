0bf3c917d38b8d89948630cfb1423107fcffcf97
韩志超 <hanzhichao@secoo.com>
2020-03-22 03:01:07 +0800

    backup longteng15

--- run.py
+++ run.py
@@ -1,17 +1,24 @@
 import os
-import time
-from util.config import Config
-from util.e_mail import send_email
-import pytest
+import logging
+import unittest
+import click
+from config import basedir
+from lib.htmlrunner import HTMLTestRunner
+from lib.mail import send_email
 
-def main():
-    cf = Config()
-    report_dir = cf.get_report_dir()
-    now = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    report_name = os.path.join(report_dir, 'report_' + now + '.html')
-    pytest.main(["-q", "case", "--html=" + report_name])
-    send_email(report_name)
+
+@click.command()
+@click.option("--send", help='是否发送邮件')
+def run_all(send):
+    logging.info("====================================== 测试开始 ======================================")
+    suite = unittest.defaultTestLoader.discover(os.path.join(basedir, "testcase"))
+    report_file = os.path.join(basedir, 'report', "report.html")
+    with open(report_file, "wb") as f:
+        HTMLTestRunner(stream=f, title="自动化测试报告", description="龙腾15期加油卡接口测试").run(suite)
+    logging.info("====================================== 测试结束 ======================================")
+    if send == "true":
+        send_email(report_file)
 
 
 if __name__ == '__main__':
-    main()
+    run_all()
